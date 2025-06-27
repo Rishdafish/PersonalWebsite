f9920e24c4492708972192f8574b69cd4877b1a2
Rishdafish <128328523+Rishdafish@users.noreply.github.com>
2025-06-27 12:26:43 -0400

    Fix StarField import error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { StarField } from './components/StarField';
+import StarField from './components/StarField';
 import { ConnectionErrorBanner } from './components/ConnectionErrorBanner';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
